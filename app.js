df6cfee1743d44ab6accb0f0fb13528022dd3b3e
NAIF IBRAHIM <naifibrahim125@gmal.com>
2023-09-22 13:39:53 -0700

    practice work

--- app.js
+++ app.js
@@ -156,8 +156,32 @@
 // var d = new Date (1695311417291)
 // console.log(d.toLocaleString())
 
+// new Date().toLocaleDateString(); /Give date only
+// new Date().toLocaleTimeStringString(); /give time only
+// new Date().toLocaleString(); /give date & time both
 
+// MATH OBJECTS
+// console.log(Math.PI) /Give PI value
 
+// let num = 10.500
+// console.log(Math.round(num)) /gives round number to its nearest integer
 
+// console.log(Math.pow(2,3)) gives power value 2x2x2
+// console.log(2**3) /gives power value 2x2x2
 
+// console.log(Math.sqrt(25)) / ----------------
+// console.log(Math.sqrt(81)) / GIVES SQUAREROOT 
+// console.log(Math.sqrt(65)) / ----------------
 
+// console.log(Math.abs(-45))  --------------------
+// console.log(Math.abs(-108)) GIVES POSITIVE VALUE
+// console.log(Math.abs(-100)) --------------------
+
+// console.log(Math.ceil(4.01)) /INCREASE .VALUE BY 1
+// console.log(Math.floor(4.7)) /REMOVES THE .VALUE
+// console.log(Math.min(4,5,6,8,10,-200)) /GIVES MINIMUM VALUE
+// console.log(Math.max(4,5,6,8,10,-200)) /GIVES MAXIMUM VALUE
+// console.log(Math.floor(Math.random()*10)) /GIVE RANDOM NUMBERS LESS THAN 10 
+// console.log(Math.trunc(4.7))----------------------   
+// console.log(Math.trunc(-5.8)) /GIVES INTEGER PART OF A NUMBER
+// console.log(Math.trunc(-10.11))-------------------
